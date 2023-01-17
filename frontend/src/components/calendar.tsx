0497012a5b599b26aaf37983e47de9b9cca0a9d4
Anthony <anthony.tuo.liu@gmail.com>
2023-01-17 23:31:21 +1100

    build calendar object for frontend update fixture to suit for the first week

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Availability } from '../data/availability';
+import { Availability } from "../data/availability";
 import { DateTime } from "luxon";
+import styles from "./calendar.module.css";
 
 type CalendarProps = {
   year: string;
@@ -19,13 +20,47 @@ const Calendar = (props: CalendarProps) => {
     weekNumber: weekNumber
   });
 
-  const weekStart = dt.startOf('week');
-  console.log(weekStart)
+  const availabilityTimeFormat = 'yyyyMMddHH';
 
-  // display the calendar
+  const weekStartTime = dt.startOf('week');
+  console.log(weekStartTime.toUTC().toFormat(availabilityTimeFormat));
+  // const weekStartTimeStr = parseInt(weekStart.toUTC().toFormat(availabilityTimeFormat));
+
+  // build calendar object
+  let weekTimes: {[key: number]: string} = {};
+
+  // loop hours, if break down to 15 mins, this logic need to update
+  for (let t = 0; t < 168; t++) {
+    console.log(`t: ${t}`)
+    const timeString = weekStartTime.plus({ hours: t }).toFormat(availabilityTimeFormat);
+
+    console.log(`timeString: ${timeString}`)
+
+    const availabilityStatus = availability[timeString] ?? 'n';
+    console.log(availabilityStatus);
+    weekTimes[timeString] = availabilityStatus;
+  }
+
+  /**
+   * display the calendar
+   * Because this is a highly customized table then I just use pure html table
+   */
   return <div>
     <h2>Availability Calendar</h2>
     {JSON.stringify(availability)}
+    {JSON.stringify(weekTimes)}
+    <table className={styles.table}>
+      <th>
+        <td>Mon</td>
+        <td>Tue</td>
+        <td>Wed</td>
+        <td>Thu</td>
+        <td>Fri</td>
+        <td>Sat</td>
+        <td>Sun</td>
+      </th>
+
+    </table>
     </div>
 }
 
